19e36d63453c85ce00b9f759585aca19aa1d8a2f
dipesh chaulagain <dipesh@heubert.com>
2022-09-22 22:59:31 +0545

    login api call tested with mock service worker

--- src/components/forms/LoginForm.spec.tsx
+++ src/components/forms/LoginForm.spec.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { mockTestMatchMedia } from "../../testUtils/mock_test_matchMedia";
 import LoginForm from "./LoginForm";
@@ -26,7 +26,7 @@ describe("Login Form", () => {
     expect(errorMsg).toBeInTheDocument();
   });
 
-  test.each`
+  it.each`
     field          | message                                    | value
     ${"user_name"} | ${"Username must be 5-20 characters long"} | ${"dip"}
     ${"user_name"} | ${"Username must be 5-20 characters long"} | ${"thequickbrownfoxjumpedoveralazydog"}
@@ -42,7 +42,7 @@ describe("Login Form", () => {
     }
   );
 
-  test.each`
+  it.each`
     field         | message                                                                      | value
     ${"password"} | ${"Password must be at least 8 characters long"}                             | ${"dip"}
     ${"password"} | ${"Make sure to have one number, one capital letter and one special symbol"} | ${"nepal#8848"}
@@ -52,11 +52,30 @@ describe("Login Form", () => {
     "Should show $message when incorrect input $value",
     async ({ field, message, value }) => {
       render(<LoginForm />);
-      const userName = await screen.findByTestId(field);
-      userEvent.type(userName, value);
+      const password = await screen.findByTestId(field);
+      userEvent.type(password, value);
 
       const errorMsg = await screen.findByText(message);
       expect(errorMsg).toBeInTheDocument();
     }
   );
+
+  it("should call login api when form is submmited with valid inputs keeping submit button disabled until response comes", async () => {
+    render(<LoginForm />);
+    const userName = await screen.findByTestId("user_name");
+    const password = await screen.findByTestId("password");
+    const submitBtn = await screen.findByTestId("submit_btn");
+    userEvent.type(userName, "dipesh chaulagain");
+    userEvent.type(password, "Nepal#8848");
+    userEvent.click(submitBtn);
+    await waitFor(() => {
+      expect(submitBtn).toBeDisabled();
+    });
+
+    await waitFor(async () => {
+      const msg = await screen.findByTestId("msg");
+      expect(msg).toBeInTheDocument();
+    });
+    expect(submitBtn).toBeEnabled();
+  });
 });
